71fe646a2da464e3e26b5e0d733481cd94c51c03
shimna <shimnaa.arangil@gmail.com>
2020-09-24 16:35:12 +0530

    render burger dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,25 @@ import styles from './Burger.module.css'
 import Burgeringredient from './BurgerIngredients/BurgerIngredients';
 
 
-const burger = () => {
+const burger = (props) => {
 
+  
+    let transingredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i)=> {
+            return <Burgeringredient key={igKey + i} type={igKey}/>;
+        });
+    })
+    .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transingredients.length === 0 ){
+        transingredients=<p>Please start adding ingredients</p>
+    }
     return(
         <div className={styles.Burger}>
             <Burgeringredient type='bread-top' />
-            <Burgeringredient type='meat' />
-            <Burgeringredient type='cheese' />
+            {transingredients}
             <Burgeringredient type='bread-bottom' />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuider.js
+++ src/containers/BurgerBuilder/BurgerBuider.js
@@ -4,10 +4,18 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+    state = {
+        ingredient: {
+            salad: 0,
+            meat: 0,
+            cheese: 0,
+            bacon: 0
+        }
+    }
     render(){
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredient}/>
                 <div>Burger Build Controls</div>
             </Aux>
         );
